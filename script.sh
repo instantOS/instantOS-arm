b5cc38f039414a1c699b8e13c391904dec68d3bc
Simon Gardling <titaniumtown@gmail.com>
2020-07-13 18:36:54 +0000

    auto-uncomment options for the gpu

--- script.sh
+++ script.sh
@@ -10,7 +10,8 @@ cp -v pacman.conf target_root/etc/
 cp -v makepkg.conf target_root/etc/
 #temp copy internal script to chroot:
 cp internal_script.sh target_root/
-
+sed -i -e 's/#dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d/g' target_root/boot/config.txt
+sed -i -e 's/#gpu_mem=256/gpu_mem=256/g' target_root/boot/config.txt
 #mount --bind
 mount --bind target_root target_root
 arch-chroot target_root "./internal_script.sh"
