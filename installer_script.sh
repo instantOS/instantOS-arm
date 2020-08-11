3b2aad9cc479e20ba4fd04060c701ed4a4b00a51
hen6003 <henrysmith6003@gmail.com>
2020-08-11 03:12:46 +0100

    made drive chooser show info about the drives

--- installer_script.sh
+++ installer_script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-drive=$(lsblk -n | awk '!/─/' | awk '{ print $1 }' | fzf)
+rm /tmp/raspiinstaller
 
-echo $drive
+drives=$(lsblk -n | awk '!/─/' | awk '{ print $1 }')
+
+for item in $drives;
+do
+		echo $item $(lsblk -dn /dev/sda | awk '{ print $4 }') $(cat /sys/block/$item/device/model) >> /tmp/raspiinstaller
+done
+
+drive=$(cat /tmp/raspiinstaller | fzf)
+
+echo Are you sure this is the right drive: $drives?
+
+#read right
+
+
