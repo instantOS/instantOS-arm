97e8d090ee298b317b151574e5e3f76e5216846e
hen6003 <henrysmith6003@gmail.com>
2020-08-18 20:20:37 +0100

    disk size now changes for the disk

--- installer_script.sh
+++ installer_script.sh
@@ -7,7 +7,7 @@ drives=$(lsblk -n | awk '!/─/' | awk '{ print $1 }')
 
 for item in $drives;
 do
-		echo $item $(lsblk -dn /dev/sda | awk '{ print $4 }') $(cat /sys/block/$item/device/model) >> /tmp/raspiinstaller
+		echo $item $(lsblk -dn /dev/"$item" | awk '{ print $4 }') $(cat /sys/block/$item/device/model) >> /tmp/raspiinstaller
 done
 
 drive=$(cat /tmp/raspiinstaller | fzf)
