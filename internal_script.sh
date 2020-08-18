56a5cc1e678e323dc98e71812f8ceb14d365e5fc
Simon Gardling <titaniumtown@gmail.com>
2020-08-18 14:23:58 -0400

    remove lightdm related stuff

--- internal_script.sh
+++ internal_script.sh
@@ -2,24 +2,11 @@
 pacman-key --init
 pacman-key --populate archlinuxarm
 
-if grep -q 'greeter-session' /etc/lightdm/lightdm.conf; then
-    LASTSESSION="$(grep 'greeter-session' /etc/lightdm/lightdm.conf | tail -1)"
-    sed -i "s/$LASTSESSION/greeter-session=lightdm-gtk-greeter/g"  /etc/lightdm/lightdm.conf
-else
-	sed -i 's/^\[Seat:\*\]/\[Seat:\*\]\ngreeter-session=lightdm-gtk-greeter/g' /etc/lightdm/lightdm.conf
-fi
-
-
-if grep -q '^user-session.*' /etc/lightdm/lightdm.conf; then
-    echo "adjusting user session"
-    sed -i 's/^user-session=.*/user-session=instantwm/g' /etc/lightdm/lightdm.conf
-fi
 
 sed -i 's/^#logind-check-graphical=.*/logind-check-graphical=true/' /etc/lightdm/lightdm.conf
 sed -i 's/# %wheel/%wheel/g' /etc/sudoers
 
 #enable services
-# systemctl enable lightdm
 systemctl enable NetworkManager
 systemctl enable systemd-timesyncd
 
