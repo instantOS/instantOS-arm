461dc0bbacfafeb8821b3140a62a214fda613fb3
Simon Gardling <titaniumtown@gmail.com>
2020-08-18 14:30:49 -0400

    add check for root and architecture

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+(( $(id -u) == 0 )) || { echo >&2 "Must be root to run script"; exit 1; }
+(( $(uname -m) != "aarch64" )) || { echo >&2 "sorry, this script only supports aarch64 at the moment"; exit 1; }
 #Note: Run this script on a raspberry pi 4, to generate a functioning tar ball
 mkdir target_root
 kernel="linux-raspberrypi4-5.4.y"
