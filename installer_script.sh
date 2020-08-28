d8ff470679ad853433bc320db525d77dd12173c3
hen6003 <henrysmith6003@gmail.com>
2020-08-28 05:03:12 +0100

    added formating card

--- installer_script.sh
+++ installer_script.sh
@@ -11,9 +11,20 @@ do
 done
 
 drive=$(cat /tmp/raspiinstaller | fzf || echo sda)
+disk=$(echo $drive | sed 's/\s.*$//')
 
-echo Are you sure this is the right drive: $(echo $drive | sed 's/\s.*$//' )?
-
+echo Are you sure this is the right drive: "$disk"[y/N]?
 read right
+
 rm /tmp/raspiinstaller > /dev/null 2>&1
 
+if [ "$right" != "y" ]; then
+    exit
+fi
+
+echo Formating
+parted --script /dev/"$disk" -- \
+        mklabel gpt \
+        mkpart esp fat32 1MiB 1GiB \
+        mkpart primary 1GiB 100% \
+        set 1 boot on
