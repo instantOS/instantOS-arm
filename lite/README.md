8bd7fd5ccac31fc09cbfd7e87a9be5325e6dcee6
Simon Gardling <titaniumtown@gmail.com>
2021-02-04 15:32:04 -0500

    more progress

--- lite/README.md
+++
@@ -1,3 +0,0 @@
-# instantOS arm lite
-
-instantOS arm without the window manager. It can be used for aarch64 Raspberry Pi 4s.
--- script.sh
+++ script.sh
@@ -1,29 +1,93 @@
 #!/bin/bash
-(( $(id -u) == 0 )) || { echo >&2 "Must be root to run script"; exit 1; }
+
+set -e # exit script if any command fails
+
+# Options:
+targetDevice="rpi4" # target device (doesn't do anything yet)
+buildType="full" # specify build type here, full = gui and everything, lite = full without gui
+silentPacstrap="False" # specifies whether or not pacstrap outputs anything to the terminal
+
+# Print options:
+echo "Config:"
+echo "   Target device: ${targetDevice}"
+echo "   Build type: ${buildType}"
+echo "   Silenced pacstrap: ${silentPacstrap}"
+
+
+source "$(pwd)/scripts/spinner.sh" # for spinners
+
+targetChroot="target_root"
+
+if [[ $(id -u) != 0 ]]; then echo >&2 "Must be root to run script"; exit 1; fi
 arch=$(uname -m | tr -d '\n')
-if [[ "$arch" != "aarch64" ]]; then echo >&2 "sorry, this script only supports aarch64 at the moment"; fi
-#Note: Run this script on a raspberry pi 4, to generate a functioning tar ball
-mkdir target_root
+if [[ "$arch" != "aarch64" ]]; then echo >&2 "sorry, this script only supports aarch64 at the moment"; exit 1; fi
+# Note: Run this script on a raspberry pi 4, to generate a functioning tarball
+
+if [ -d "${targetChroot}" ]; then rm -rf "${targetChroot}"; fi
+
+mkdir "${targetChroot}"
 kernel="linux-raspberrypi4-5.4.y"
-pacstrap -C pacman.conf target_root base base-devel networkmanager xdg-user-dirs nitrogen ${kernel} ${kernel}-headers raspberrypi-firmware mesa mesa-vdpau rofi-git lxsession dunst alsa-utils raspberrypi-bootloader xorg-xinit xorg-server
+
+date=$(date +"%F" | tr -d '\n')
+echo -e "date: ${date}\n"
+
+tarName="instantOS-arm-raspi4-${date}"
+
+basePackages="base base-devel networkmanager ${kernel} ${kernel}-headers raspberrypi-firmware raspberrypi-bootloader" # packages included in all builds
+instantoOSPackages="grub-instantos instantassist instantclipmenu instantconf instantcursors instantdotfiles instantextra instantfonts instantlaunch instantlock instantmenu instantnotify instantos instantpacman instantsearch instantsettings instantshell instantsupport instantthemes instanttools instantupdate instantutils instantwallpaper instantwelcome instantwidgets instantwm plymouth-theme-instantos st-instantos ufetch-instantos" # instantos-specific packages - TODO: remove grub dependency as it's unneeded on raspberry pis
+fullPackages="${basePackages} mesa mesa-vdpau rofi-git lxsession dunst alsa-utils raspberrypi-bootloader xorg-xinit xorg-server ${instantosPackages}" # packages for the full build
 
 
-rm -fr target_root/etc/pacman.conf target_root/etc/makepkg.conf
-cp -v pacman.conf target_root/etc/
-cp -v makepkg.conf target_root/etc/
-#temp copy internal script to chroot:
-cp internal_script.sh target_root/
-#mount --bind
-mount --bind target_root target_root
-arch-chroot target_root "./internal_script.sh"
-rm target_root/internal_script.sh
-sed -i -e 's/#dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d/g' target_root/boot/config.txt
-sed -i -e 's/#gpu_mem=256/gpu_mem=256/g' target_root/boot/config.txt
+if [[ "${buildType}" == "full" ]]; then
+    packageList="${fullPackages}"
+    tarName="${tarName}-full"
+elif [[ "${buildType}" == "lite" ]]; then
+    packageList="${basePackages}"
+    tarName="${tarName}-lite"
+else
+    echo "ERROR: variable buildType is invalid"
+    exit 1
+fi
 
+
+if [[ "$silentPacstrap" == "False" ]]; then
+    pacstrap -C "chroot_things/pacman.conf" "${targetChroot}" ${packageList}
+elif [[ "$silentPacstrap" == "True" ]]; then
+start_spinner 'Running pacstrap...'
+    pacstrap -C "chroot_things/pacman.conf" "${targetChroot}" ${packageList} 2>&1
+    stop_spinner $?
+else
+    echo "ERROR: variable silentPacstrap is invalid"
+    exit 1
+fi
+
+
+start_spinner 'Copying files...'
+rm -fr "${targetChroot}/etc/pacman.conf" "${targetChroot}/etc/makepkg.conf"
+
+cp -v "chroot_things/pacman.conf" "${targetChroot}/etc/pacman.conf"
+cp -v "chroot_things/makepkg.conf" "${targetChroot}/etc/makepkg.conf"
+# temp copy internal script to chroot:
+cp "chroot_things/internal_script_${buildType}.sh" "${targetChroot}/internal_script.sh"
+stop_spinner $?
+
+
+mount --bind "${targetChroot}" "${targetChroot}" # Has to be like this or else pacstrap isn't happy
+start_spinner 'Running internal script...'
+arch-chroot "${targetChroot}" "./internal_script.sh"
+rm "${targetChroot}/internal_script.sh"
+stop_spinner $?
+
+if [[ "${buildType}" == "full" ]]; then
+    sed -i -e 's/#dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d/g' "${targetChroot}/boot/config.txt"
+    sed -i -e 's/#gpu_mem=256/gpu_mem=256/g' "${targetChroot}/boot/config.txt"
+fi
 
 #un-bind directory
-umount target_root
+umount "${targetChroot}"
 
-cd target_root
-tar -czvf ../instantOS-arm-raspi4.tar.gz .
+start_spinner "Compressing final tarball..."
+cd "${targetChroot}"
+tar -I pzstd -cf "../${tarName}.tar.zst" .
 cd ..
+stop_spinner $?
